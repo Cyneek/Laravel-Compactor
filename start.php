650129602b2cf9b9dc280053bcebd1f4da08daf2
Patroklo <patroklo@gmail.com>
2012-09-30 12:54:06 +0300

    Update start.php

--- start.php
+++ start.php
@@ -18,12 +18,12 @@
  * @package     Laravel-Compactor
  * @author      Eric Barnes
  * @author      Jeroen van Meerendonk
- * @author      Joseba Juaniz
+ * @author      Joseba Juániz
  * @copyright   Copyright (c) Eric Barnes. (http://ericlbarnes.com/)
  * @copyright   Copyright (c) Cyneek. (http://cyneek.com)
  * @license     http://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
  * @link        http://cyneek.com
- * @since       Version 1.0
+ * @since       Version 1.2
  * @filesource
  *
  */
